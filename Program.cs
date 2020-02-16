540107397569a7a9a66d89f4598693c786ef0f30
kobi kahila <kobik69@gmail.com>
2020-02-16 23:36:25 +0200

    first try using git hub
    16/2/2020

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace basic_lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Console.Write("Enter number fo digit check: ");
+            //int x = Convert.ToInt32(Console.ReadLine());
+            //if (x >= 0 && x <= 9)
+            //{
+            //    // YES
+            //    Console.WriteLine("1 digit");
+            //}
+            //else if (x >= 10 && x <= 99)
+            //{
+            //    // YES
+            //    Console.WriteLine("2 digits");
+            //}
+            //else
+            //{
+            //    // Everything before was not true
+            //    Console.WriteLine("3 or more digits...");
+            //}
+            Console.Write("please enter  any number:");
+            int num = int.Parse(Console.ReadLine());
+
+            if (num != 0)
+            {
+                int digit1 = num % 10; 
+                Console.WriteLine("1", digit1);
+              
+            }
+            else if (num>10&& num<=99)
+            {
+            
+            Console.WriteLine("2" );
+            }
+            else
+            {
+                Console.WriteLine("number  bigger than 99 " );    
+            }
+           
+            
+        }
+    }
+}
